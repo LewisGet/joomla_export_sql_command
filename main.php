15296560847d3d53a766e29827611303104c1095
Lewis Jang <lewis.aedes@gmail.com>
2018-06-21 16:27:26 +0800

    init

---
+++ main.php
@@ -0,0 +1,14 @@
+<?php
+
+$config_path = $argv[1];
+
+require_once($config_path);
+
+$config = new JConfig();
+
+$usersname = $config->user;
+$passworld = $config->passworld;
+$databases = $config->db;
+$timestamp = time();
+
+echo "mysqldump -u {$usersname} -p {$databases} > {$databases}-{$timestamp}.sql"
